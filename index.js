e37326f9f2b0724150a1893549447272fce2cdc3
= <=>
2024-03-16 12:17:20 -0300

    feat: add data lib

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import exphbs from "express-handlebars"; 
+import { products } from "./lib/data.js"; 
 
 const app = express();
 const port = 3000; 
@@ -19,14 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-    const products = [
-        {id: 1, name: "Gaming Chair", price: 302.99, description: "Chair for gamers." },
-        {id: 2, name: "Gaming Mouse", price: 30.99, description: "Mouse for gamers." },
-        {id: 3, name: "Gaming Keyboard", price: 47.50, description: "Keyboard for gamers." },
-        {id: 4, name: "Headset", price: 45.00, description: "Headset for gamers.", link: `/products/4` },
-        {id: 5, name: "Complete Gabinet Desktop", price: 2200.00, description: "Complete gabinet including motherboard, Intel Core i9 CPU, 32GB RAM, 70GB SSD, 2TB HDD." },
-        {id: 6, name: "Desktop Monitor", price: 200.00, description: "25 inch Desktop Monitor, 4k resolution." }
-    ]
+    
     res.render("products", { products }); 
 });
 
@@ -41,7 +35,9 @@ app.get("/products/:id", (req, res) => {
         return ;
     }
 
-    res.render("product-single");
+    const product = products[id - 1]
+
+    res.render("product-single", { product });
 })
 
 
